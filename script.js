9446015970405f494ca852115478e1dfe79c62bd
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2024-02-26 21:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function createDom(root) {
+if (typeof root === 'string') {
+    return document.createTextNode(root);
+  }
 
+  const element = document.createElement(root.type);
+
+  if (root.attributes) {
+    Object.keys(root.attributes).forEach(attr => {
+      element.setAttribute(attr, root.attributes[attr]);
+    });
+  }
+
+  if (root.children) {
+    root.children.forEach(child => {
+      element.appendChild(createDom(child));
+    });
+  }
+
+  return element;
 }
 
 module.exports=createDom
